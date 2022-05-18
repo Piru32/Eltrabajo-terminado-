e95212ba5e8714f8fb3a6909d60afaeb01c07f9f
juan-zapata-2006 <juanjosezapatamosquera@gmail.com>
2022-05-18 17:15:33 -0500

    se termina

---
+++ controlador.js
@@ -0,0 +1,516 @@
+//OBJETOS
+let producto1={
+    nombre:"Pantalon 1",
+    precio:74.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen1.jpg?alt=media&token=66650e0a-6e5c-4a21-b685-890d46e30781"
+}
+
+let producto2={
+    nombre:"Pantalon 2",
+    precio:49.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen2.jpg?alt=media&token=439711fe-3c0e-4b25-8386-0622a5e28730"
+}
+
+let producto3={
+    nombre:"Camiseta 1",
+    precio:69.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen3.jpg?alt=media&token=55d6cb19-efcf-4e8a-9548-a96624c5e45c"
+}
+
+let producto4={
+    nombre:"Camiseta 2",
+    precio:69.990,
+    estado: true,
+foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen4.jpg?alt=media&token=03671de3-20ab-4648-8e2b-1956db0fcada"
+}
+
+let producto5={
+    nombre:"Camiseta 5",
+    precio:69.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/camiseta%205.jpg?alt=media&token=61f8438d-95cd-44ab-bb7b-72265b8a885f"
+}
+
+let producto6={
+    nombre:"Buzo 2",
+    precio:59.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen6.jpg?alt=media&token=f225c9df-cb3d-46e0-975b-c968e4db331e"
+}
+
+let producto7={
+    nombre:"Camiseta 3",
+    precio:39.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen8.jpg?alt=media&token=c16f1b7a-2ad4-4c55-a385-74ca6e36ef99"
+}
+
+let producto8={
+    nombre:"Camiseta 4",
+    precio:74.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen7.jpg?alt=media&token=00d2e8fa-eef7-459e-8225-34d486fa559c"
+}
+
+let producto9={
+    nombre:"Short 1",
+    precio:29.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen9.jpg?alt=media&token=7761499a-ac33-4b4f-83ec-45294630fd0a"
+}
+
+let producto10={
+    nombre:"Short 2",
+    precio:29.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen10.jpg?alt=media&token=f41e6ed1-db86-48c8-a06f-e8f9ec717192"
+}
+
+let producto11={
+    nombre:"Tenis 1",
+    precio:139.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen11.jpg?alt=media&token=df7231ca-7e11-4103-af19-e1eae7b23901"
+}
+
+let producto12={
+    nombre:"Tenis 2",
+    precio:139.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen12.jpg?alt=media&token=7a92d085-374a-4995-9bd6-458e979c53e2"
+}
+
+let producto13={
+    nombre:"Tenis 3",
+    precio:159.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen13.jpg?alt=media&token=65f92574-c09c-4dab-b3c6-ff8ce8cbc281"
+}
+
+let producto14={
+    nombre:"Tenis 4",
+    precio:159.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen14.jpg?alt=media&token=8b1164cb-70c3-4af6-9285-5ab0fbeca09b"
+}
+
+let producto15={
+    nombre:"Medias 1",
+    precio:6.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen15.jpg?alt=media&token=85078262-d114-41eb-a2ec-a5906a830d09"
+}
+
+let producto16={
+    nombre:"Tenis 5",
+    precio:159.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/Tenis%205.jpg?alt=media&token=8c18959f-5311-4b5d-b101-e06b8b3dff81"
+}
+let producto17={
+    nombre:"Medias 2",
+    precio:5.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen17.jpg?alt=media&token=d197c80f-070b-4687-b3df-d5851b7f52b7"
+}
+let producto18={
+    nombre:"Correa 1",
+    precio:19.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen18.jpg?alt=media&token=4ddd2cfa-30f9-43dc-857e-46652e1c3299"
+}
+let producto19={
+    nombre:"Cadena 1",
+    precio:29.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen19.jpg?alt=media&token=b2d245b8-73f9-454b-944c-71ca59393f1d"
+}
+let producto20={
+    nombre:"Anillos 1",
+    precio:19.990,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen20.jpg?alt=media&token=c1734abd-afa6-476c-9cb9-bc5781ca2c0b"
+}
+let producto21={
+    nombre:"Pantaloneta",
+    precio: 25.990,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/pantaloneta%201.jpg?alt=media&token=f363a847-af7c-4b5a-be6a-32ad9a0213b2",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto22={
+    nombre:"Pantaloneta",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/pantaloneta%202.jpg?alt=media&token=5c15463f-11ec-4cfd-95ff-df877507a776",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto23={
+    nombre:"Pantaloneta",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/pantaloneta%203.jpg?alt=media&token=c878eef8-c610-427f-b9d7-56f99961133a",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto24={
+    nombre:"Pantaloneta",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/pantaloneta%204.jpg?alt=media&token=58100bd0-2b2a-41ae-b49f-03a2d40837b4",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto25={
+    nombre:"Pantaloneta",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/pantaloneta%205.jpg?alt=media&token=91237c5c-663b-4316-981d-d9fbd8e8cf7b",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto26={
+    nombre:"Anillo 2",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/anillo%202.jpg?alt=media&token=15fae90c-5b71-4c7d-abbc-77213a5813a6",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto27={
+    nombre:"Anillo 3",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/anillo%203.jpg?alt=media&token=ac086843-2e8d-4729-bf9f-4d43c3c2c5e9",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto28={
+    nombre:"Anillo 4",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/anillo%204.jpg?alt=media&token=81902746-78c2-4f7a-9a42-852cdc311170",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto29={
+    nombre:"Buso 3",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/buso%205.jpg?alt=media&token=081fa267-a2f6-4968-9a6b-8e14a8ea7c748",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto30={
+    nombre:"Anillo 5",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/anillo%205.jpg?alt=media&token=5fbe5be4-973e-4651-88d1-bd44a18e70c4",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto31={
+    nombre:"Buso 4",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/buso%203.jpg?alt=media&token=d39873c6-48d6-41bb-8f76-08f8ada8e251",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto32={
+    nombre:"Buso 5",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/buso%204.jpg?alt=media&token=35bdbbaf-0334-4073-89e6-164b166da3f9",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto33={
+    nombre:"Cadena 2",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/cadena%202.jpg?alt=media&token=897faadf-97ad-4fae-802c-d3dc0c1a0212",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto34={
+    nombre:"Cadena 3",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/cadena%203.jpg?alt=media&token=c55d8b50-3975-4fb1-b230-e25c337df47f",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto35={
+    nombre:"Cadena 4",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/cadena%204.jpg?alt=media&token=c3513a41-14a4-48ac-890b-b5be99b7d389",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto36={
+    nombre:"Cadena 5",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/cadena%205.jpg?alt=media&token=60e85b52-2a4b-4941-a248-ba15b33facaf",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto37={
+    nombre:"Correa 2",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/correa%202.jpg?alt=media&token=3fe4e77e-da0e-46b7-a154-98488478cd2b",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto38={
+    nombre:"Correa 3",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/correa%203.jpg?alt=media&token=1090f15b-7d7c-4c00-88c4-2471f90f65a9",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto39={
+    nombre:"Correa 4",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/traje_ba%C3%B1o-1.jpg?alt=media&token=570573df-fcb1-41cf-901a-f6f47d0939e8",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto40={
+    nombre:"Correa 5",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/correa%204.jpg?alt=media&token=38a83510-1925-4fcc-a89e-fb60de329df6",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto41={
+    nombre:"Gorra 2",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/gorra%202.jpg?alt=media&token=c810149e-96f6-4f70-911f-76ca6d32482b",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto42={
+    nombre:"Gorra 3",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/gorra%203.jpg?alt=media&token=8a3f21b1-2cbe-423f-a190-ef891ac5af6d",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto43={
+    nombre:"Gorra 4",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/gorra%204.jpg?alt=media&token=92a02f8d-f017-4f00-9005-c5aa96539c59",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto44={
+    nombre:"Gorra 5",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/gorra%205.jpg?alt=media&token=5e24ef06-9292-4581-93a1-d01d07fb5490",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto45={
+    nombre:"",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/traje_ba%C3%B1o-1.jpg?alt=media&token=570573df-fcb1-41cf-901a-f6f47d0939e8",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto46={
+    nombre:"",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/traje_ba%C3%B1o-1.jpg?alt=media&token=570573df-fcb1-41cf-901a-f6f47d0939e8",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto47={
+    nombre:"",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/traje_ba%C3%B1o-1.jpg?alt=media&token=570573df-fcb1-41cf-901a-f6f47d0939e8",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto48={
+    nombre:"",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/traje_ba%C3%B1o-1.jpg?alt=media&token=570573df-fcb1-41cf-901a-f6f47d0939e8",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto49={
+    nombre:"",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/traje_ba%C3%B1o-1.jpg?alt=media&token=570573df-fcb1-41cf-901a-f6f47d0939e8",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+
+let producto50={
+    nombre:"",
+    precio: 25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-del-guetto.appspot.com/o/traje_ba%C3%B1o-1.jpg?alt=media&token=570573df-fcb1-41cf-901a-f6f47d0939e8",
+    popularidad: 5,
+    pais: "Colombia" 
+}
+//★
+
+
+//ARREGLO
+let productos=Array(
+    producto1,
+    producto2,
+    producto3,
+    producto4,
+    producto5,
+    producto6,
+    producto7,
+    producto8,
+    producto9,
+    producto10,
+    producto11,
+    producto12,
+    producto13,
+    producto14,
+    producto16,
+    producto15,
+    producto17,
+    producto18,
+    producto19,
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50
+
+)
+
+let contenedor = document.getElementById("contenedor")
+
+//RECORRO EL ARREGLO
+productos.forEach(function(producto){
+    let foto = document.createElement("img")
+    foto.classList.add("w-100", "img-fluid", "mt-2")
+    foto.src = producto.foto
+    
+    let columna = document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta = document.createElement("div")
+    tarjeta.classList.add("card")
+
+    let nombreProducto = document.createElement("h4")
+    nombreProducto.classList.add("text-center")
+    nombreProducto.textContent = producto.nombre
+
+    let precioProducto = document.createElement("p")
+    precioProducto.classList.add("card-text", "text-primary")
+    precioProducto.textContent = "$" + producto.precio
+
+    let estadoProducto = document.createElement("p")
+    estadoProducto.classList.add("card-text", "text-success")
+    estadoProducto.textContent = producto.estado 
+    
+    let popuProducto = document.createElement("p")
+    popuProducto.classList.add("text-danger")
+    popuProducto.textContent = "Popularidad: " + producto.popularidad
+
+    //tarjeta.appenChild
+    tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precioProducto)
+    tarjeta.appendChild(estadoProducto)
+    tarjeta.appendChild(popuProducto)
+
+    columna.appendChild(tarjeta)
+    contenedor.appendChild(columna)
+
+    //Condicion =true
+    if(producto.estado == true){
+        estadoProducto.classList.add("text-secondary")
+        estadoProducto.textContent = "Estado: Nuevo"
+    }
+
+    else{
+        estadoProducto.classList.add("text-secondary")
+        estadoProducto.textContent = "Estado: Usado"
+    }
+
+})
+
